3095247d6f2fe04b4e9d03e3a1dc3d776474f2b4
Kangsi <weikang_hu90@hotmail.com>
2019-01-25 21:36:51 +0100

    Collision with velocity added to the slugs.

--- src/sprites/slug.js
+++ src/sprites/slug.js
@@ -1,21 +1,22 @@
-import {Point} from 'phaser';
+import { Point } from 'phaser';
 import Sprite from '../services/sprite';
 import Controller from '../services/Controller';
 import Config from '../config';
 
 export default class Slug extends Sprite {
   constructor(playerNumber, position) {
-    super({asset: 'cloud'});
+    super({ asset: 'cloud' });
 
     this.x = position[0];
     this.y = position[1];
+
     game.physics.arcade.enable(this);
     this.body.enable = true;
     this.body.setCircle(100, 0, 0);
     this.body.bounce.set(1);
     this.body.collideWorldBounds = true;
-    this.body.immovable = true;
-    this.body.moves = false;
+    this.body.drag.setTo(100, 100)
+
 
     this.settings = Config.playerInput.player1;
     this.gamePad = this.game.input.gamepad[`pad${playerNumber}`];
@@ -28,14 +29,16 @@ export default class Slug extends Sprite {
     this.rotationSpeed = 1;
   }
 
-  onCollide() {
+  onCollide(entity1, entity2) {
+    var point = new Point()
+    const difference = Point.subtract(entity1.position, entity2.position, point).normalize();
+
+    this.body.velocity.setTo(difference.x * 100, difference.y * 100);
   }
 
   update() {
     this.controller.update();
 
-    console.log(this.currentDirection);
-    //this.rotate(0.1);
     this.x += this.targetDirection.x;
     this.y += this.targetDirection.y;
   }
