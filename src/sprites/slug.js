459ce5e328530782ae79976a52d9c97670e94371
Jenny Sun <xmissj96@gmail.com>
2019-01-25 21:22:21 +0100

    Added animations for the slug.

--- src/sprites/slug.js
+++ src/sprites/slug.js
@@ -15,6 +15,16 @@ export default class Slug extends Sprite {
     this.settings = Config.playerInput.player1;
     this.gamePad = this.game.input.gamepad.pad1;
     this.controller = new Controller(game, this, this.gamePad, this.settings);
+
+    this.createSlug();
+  }
+
+  createSlug() {
+    this.player = game.add.sprite(32, 24, 'slug', 1);
+    this.player.smoothed = false;
+
+    this.moving = this.player.animations.add('moving', [0, 3], 10, true);
+    // this.idle = this.player.animations.add('idle', [0,3], 10, true);
   }
 
   onCollide() {
@@ -23,6 +33,17 @@ export default class Slug extends Sprite {
 
   update() {
     this.controller.update();
+
+    // Checks for the movements of the gamePad stick.
+    this.checkLeft = this.gamePad.isDown(Gamepad.XBOX360_DPAD_LEFT) || this.gamePad.axis(Gamepad.XBOX360_STICK_LEFT_X) < -0.1;
+    this.checkRight = this.gamePad.isDown(Gamepad.XBOX360_DPAD_RIGHT) || this.gamePad.axis(Gamepad.XBOX360_STICK_LEFT_X) > 0.1;
+    this.checkUp = this.gamePad.isDown(Gamepad.XBOX360_DPAD_UP) || this.gamePad.axis(Gamepad.XBOX360_STICK_LEFT_Y) < -0.1;
+    this.checkDown = this.gamePad.isDown(Gamepad.XBOX360_DPAD_DOWN) || this.gamePad.axis(Gamepad.XBOX360_STICK_LEFT_Y) > 0.1;
+
+    // Checks whether gamePad is being used.
+    if (this.checkLeft || this.checkRight || this.checkUp || this.checkDown) {
+      this.player.play('moving');
+    }
   }
 
   moveUp() {
