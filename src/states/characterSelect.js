6051753243efc6579af4eec6824ea44a4c55061b
Sly Emenike <sly_emenike@hotmail.com>
2019-01-26 15:25:08 +0100

    Show correct controller plyaer order

--- src/states/characterSelect.js
+++ src/states/characterSelect.js
@@ -42,26 +42,26 @@ export default class characterSelect extends State {
           this.controllersAdded[i] = true;
           console.log('adding controller: ', i + 1);
 
-          switch (i) {
+          switch (game.totalPlayers-1) {
             case 0:
               this.screen = game.add.sprite(300, 120, 'menuCard');
               this.screens.push(this.screen);
-              game.add.text(this.screen.x, this.screen.y, `${i}`);
+              game.add.text(this.screen.x, this.screen.y, `${game.totalPlayers}`);
               break;
             case 1:
               this.screen = game.add.sprite(700, 120, 'menuCard');
               this.screens.push(this.screen);
-              game.add.text(this.screen.x, this.screen.y, `${i}`);
+              game.add.text(this.screen.x, this.screen.y, `${game.totalPlayers}`);
               break;
             case 2:
               this.screen = game.add.sprite(300, 400, 'menuCard');
               this.screens.push(this.screen);
-              game.add.text(this.screen.x, this.screen.y, `${i}`);
+              game.add.text(this.screen.x, this.screen.y, `${game.totalPlayers}`);
               break;
             case 3:
               this.screen = game.add.sprite(700, 400, 'menuCard');
               this.screens.push(this.screen);
-              game.add.text(this.screen.x, this.screen.y, `${i}`);
+              game.add.text(this.screen.x, this.screen.y, `${game.totalPlayers}`);
               break;
           }
         }
